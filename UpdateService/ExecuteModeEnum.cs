b6a29a8fb9d9b14c7604110af9f5589d040f2c49
JiaJia <gb63728f4@gmail.com>
2024-08-25 18:25:49 +0800

    feat: 新增安裝模式Enum

---
+++ UpdateService/ExecuteModeEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace UpdateService
+{
+    /// <summary>
+    /// 安裝模式
+    /// </summary>
+    public enum ExecuteModeEnum
+    {
+        Install = 0,
+        UnInstall = 1,
+        Complete = 2
+    }
+}
